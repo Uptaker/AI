189dec86fce8c03a6bafdd9e404d77b000f05ea6
Markus Tammeoja <markus@codeborne.com>
2022-09-12 17:46:25 +0300

    initial checkers commit using matrices

--- code/week1/src/App.svelte
+++ code/week1/src/App.svelte
@@ -4,14 +4,16 @@
   import UnfinishedSolution from "./components/UnfinishedSolution.svelte";
   import TeacherSomething from "./components/WithBuffer.svelte";
   import Rida6 from "./components/Rida6.svelte";
+  import Checkers from "./components/Checkers.svelte";
 
-  let component = Rida6
+  let component = Checkers
 </script>
 <div style="margin-bottom: 100px">
   <button on:click={() => component = UnfinishedSolution}>Unfinished</button>
   <button on:click={() => component = TeacherSolution}>Teacher's</button>
   <button on:click={() => component = TeacherSomething}>With buffer</button>
   <button on:click={() => component = Rida6}>Rida6</button>
+  <button on:click={() => component = Checkers}>Checkers</button>
 </div>
 <br>
 
---
+++ code/week1/src/components/Checkers.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+
+    import {onMount} from "svelte";
+
+    let startState = [[], [], [], [], [], [], [], []]
+
+    onMount(() => {
+        init()
+
+        for (let i = 0; i < startState.length; i++) {
+            console.log(startState[i].join(" "))
+        }
+            })
+    function init() {
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+                if (i == 0) startState[i][j] = 'X'
+                else if (i == 7) startState[i][j] = 'N'
+                else startState[i][j] = (j % 2 == 0) ? 'n' : 'x'
+            }
+        }
+    }
+
+</script>
