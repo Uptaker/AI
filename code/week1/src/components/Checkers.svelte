f342753af57dca96e3eb51badf8b617eb0d11683
Markus Tammeoja <markus@codeborne.com>
2022-09-19 16:16:43 +0300

    fix checker placement, return console log of game board

--- code/week1/src/components/Checkers.svelte
+++ code/week1/src/components/Checkers.svelte
@@ -1,30 +1,65 @@
 <script lang="ts">
+    import {onMount} from "svelte";
+
     let state = initState()
 
+    onMount(() => {
+        for (let i = 0; i < state.length; i++) {
+            console.log(state[i].join(" "))
+        }
+    })
+
     const isX = (value: string) => (value.toLowerCase() == 'x')
+    const isPlayer = (value: string) => value == 'X' || 'N'
 
     function initState(): string[][] {
         let startingState = [[], [], [], [], [], [], [], []]
         for (let i = 0; i < 8; i++) {
             for (let j = 0; j < 8; j++) {
                 if (i < 3) startingState[i][j] = (j % 2 == 0) ? (i %  2 == 0) ? 'n' : 'X' : (i %  2 == 1) ? 'n' : 'X'
-                else if (i > 4) startingState[i][j] = (j % 2 == 0) ? (i %  2 == 0) ? 'N' : 'x' : (i %  2 == 1) ? 'N' : 'x'
+                else if (i > 4) startingState[i][j] = (j % 2 == 1) ? (i %  2 == 0) ? 'N' : 'n' : (i %  2 == 1) ? 'N' : 'n'
                 else startingState[i][j] = (j % 2 == 0) ? (i %  2 == 0) ? 'n' : 'x' : (i %  2 == 1) ? 'n' : 'x'
             }
         }
         return startingState
     }
+
+    function move(row?: number, column?: number) {
+        if (row && column) {
+            console.log(row, column, state[row][column])
+            for (let r = row - 1; r < row + 2; r++) {
+                console.log(r)
+                for (let c = column - 1; c < column + 2; c++) {
+                    if (isX(state[row][column]) && state[r][c] == 'x') swap(r, c, row, column)
+                    else if (!isX(state[row][column]) && state[r][c] == 'x') swap(r, c, row, column)
+                }
+            }
+        }
+        console.log('move')
+    }
+
+    function color(value: string) {
+           return (value == 'x' || value == 'X' || value == 'N') ? '#99582A' : '#FFE6A7'
+    }
+
+    function swap(newRow: number, newColumn: number, oldRow: number, oldColumn: number) {
+         let oldValue = state[oldRow][oldColumn]
+         let newValue = state[newRow][newColumn]
+         state[newRow][newColumn] = oldValue
+         state[oldRow][oldColumn] = newValue
+        console.log('swapped')
+    }
 </script>
 
-<div class="board">
+<div class="board" style="margin-bottom: 5px">
     {#if state}
         <div class="row">
-            {#each state as row}
+            {#each state as row, i}
                 <div class="column">
-                    {#each row as value}
-                        <div class="square" style="background-color: {isX(value) ? '#99582A' : '#FFE6A7'}">
+                    {#each row as value, j}
+                        <div class="square" style="background-color: {color(value)}">
                             {#if value === 'X' || value === 'N'}
-                                <div class="checker" style="background-color: {isX(value) ? '#432818' : '#BB9457'}"></div>
+                                <div on:click={() => move(i, j)} class="checker" style="background-color: {isX(value) ? '#432818' : '#BB9457'}"></div>
                             {/if}
                         </div>
                     {/each}
@@ -33,6 +68,8 @@
         </div>
     {/if}
 </div>
+
+<button on:click={move}>Move</button>
 
 <style>
     .row {
@@ -49,6 +86,10 @@
         border-radius: 50%;
         height: 70%;
         width: 70%;
+    }
+
+    .checker:hover {
+        opacity: 50%;
     }
 
     .square {
