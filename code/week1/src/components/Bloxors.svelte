0d2696716ec7ed5e464ec239cfda059fe877baf0
Markus Tammeoja <markus@codeborne.com>
2022-10-03 16:59:16 +0300

    type safety and player finding

--- code/week1/src/components/Bloxors.svelte
+++ code/week1/src/components/Bloxors.svelte
@@ -9,6 +9,20 @@
     UP, DOWN, LEFT, RIGHT, ANY
   }
 
+  interface Position {
+    row: number,
+    column: number
+  }
+
+  function findPlayerPosition(): Position {
+    let row, column
+    row = state.findIndex((row) => row.find((c, index) => {
+      column = index
+      return c.type === Type.VERTICAL || c.type === Type.UP || c.type === Type.VERTICAL
+    }))
+    return {row, column}
+  }
+
   interface Tile {
     type: Type,
     wayX: Direction
@@ -28,16 +42,55 @@
 
   const state: Tile[][] = initState(stateAsStrings)
 
-  onMount(() => {
-    console.log(state)
-  })
+  /*
+  function explore() {
+    let algkoht = praeguneSeis.indexOf("S");
+    let loppkoht = praeguneSeis.indexOf("F");
+    praeguneSeis[loppkoht] = ".";
+    let hallid = [algkoht];
+    while (hallid.length > 0) {
+      let uuritav = hallid.shift(); //eemaldab esimese elemendi muutujasse
+      let vabad = leiaVabad(uuritav);
+      vabad.forEach((koht) => {
+        let vahe = koht - uuritav;
+        let symbol = "?";
+        if (vahe == -1) symbol = "p"
+        if (vahe == 1) symbol = "v"
+        if (vahe == -kp) symbol = "a"
+        if (vahe == kp) symbol = "y"
+        praeguneSeis[koht] = symbol;
+        hallid.push(koht)
+      })
+    }
+
+    //replace with o
+    const arvud = {v: -1, p: 1, a: kp, y:-kp}
+    if (praeguneSeis[loppkoht] != ".") {
+      let koht = loppkoht
+      while (koht != algkoht) {
+        let symbol = praeguneSeis[koht]
+        let uusKoht =  koht + arvud[symbol]
+        praeguneSeis[koht] = 'o'
+        koht = uusKoht
+      }
+    }
+
+    for (let nr = 0; nr < kp * kp; nr++) {
+      if (praeguneSeis[nr] in arvud) {
+        praeguneSeis[nr] = '.'
+      }
+    }
+    vastus = (praeguneSeis[loppkoht] == ".") ? "Ei" : "Jah";
+  }
+
+  */
 
   function initState(strings: string[]): Tile[][] {
     let startingState: Tile[][] = []
     strings.forEach(r => {
       const row = []
       const column = r.split("")
-      column.forEach(c => row.push({type: c} as Tile))
+      column.forEach(c => row.push({type: c, wayX: Direction.ANY, wayY: Direction.ANY, wayZ: Direction.ANY} as Tile))
       startingState.push(row)
     })
     return startingState
@@ -53,7 +106,7 @@
   }
 
   function colorByAdjacentType(row, column): string {
-    if (state[row][column - 1]?.type === Type.HORIZONTAL) return '#5FA1D5FF'
+    if (state[row]?.at(column - 1)?.type === Type.HORIZONTAL) return '#5FA1D5FF'
     else if (state[row - 1]?.at(column)?.type === Type.VERTICAL) return '#5FA1D5FF'
     else return 'white'
   }
@@ -73,6 +126,8 @@
     </div>
   {/if}
 </div>
+
+<button on:click={findPlayerPosition}>Move</button>
 
 <style>
   .row {
